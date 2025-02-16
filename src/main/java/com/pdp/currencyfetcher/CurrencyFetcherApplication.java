7be52a870c427c97154eaac1a851a303ce9c15ff
dukris <theannalios@gmail.com>
2025-02-16 16:22:27 +0300

    Initial commit

---
+++ src/main/java/com/pdp/currencyfetcher/CurrencyFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.pdp.currencyfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdp/currencyfetcher/CurrencyFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdp.currencyfetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
